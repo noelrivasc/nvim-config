6a0efb2471ca556ac15bfba70a102a931f36bd90
Noel Rivas <hola@noelr.dev>
2025-06-18 19:44:06 -0600

    Disable .git ancestor for ctrlp; use cwd instead.

--- lua/core/plugin_config/ctrlp.lua
+++ lua/core/plugin_config/ctrlp.lua
@@ -1,1 +1,7 @@
 vim.opt.wildignore:append{'*/vendor/*', '.git/*', '*/node_modules/*'}
+-- Sets the root of CtrlP to the current file, or
+-- the directory where vim was started. This is
+-- to prevent it from scanning everything under
+-- the parent that contains .git
+-- let g:ctrlp_working_path_mode = 'r' to use .git-containing ancestor
+vim.g.ctrlp_working_path_mode = 0;
