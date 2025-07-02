ae0641d22178948fbe45c0a20d304c63ed82f60d
Noel Rivas <hola@noelr.dev>
2025-07-02 12:42:54 -0600

    Removing config that is not actually needed - bad suggestion by Claude. For the record, the problem was a composer.json file present in the drupal module directory.

--- lua/core/plugin_config/lsp_config.lua
+++ lua/core/plugin_config/lsp_config.lua
@@ -1,12 +1,4 @@
 require('mason').setup()
 
--- Setup language servers.
-local lspconfig = require('lspconfig')
-
--- Configure phpactor with proper root directory detection
-lspconfig.phpactor.setup({
-  root_dir = lspconfig.util.root_pattern('composer.json', 'phpunit.xml', '.phpunit.xml', '.git', '.'),
-})
-
 vim.lsp.enable('phpactor')
 vim.lsp.enable('ts_ls')
