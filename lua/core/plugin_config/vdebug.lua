696dad8a2df4197638b8fe5ac5b17f4791c9d123
Noel Rivas <hola@noelr.dev>
2025-07-02 12:40:41 -0600

    Added code path mapping to vdebug. By default, it makes the mapping compatible with ddev.

--- lua/core/plugin_config/vdebug.lua
+++ lua/core/plugin_config/vdebug.lua
@@ -1,1 +1,22 @@
-vim.g.vdebug_options = {port = 9003};
+-- NOTE repeated function from editorconfig
+-- DRY skipped to avoid dependencies
+local function find_project_root()
+  local current_dir = vim.fn.getcwd()
+  local dir = current_dir
+
+  while dir ~= "/" do
+    if vim.fn.isdirectory(dir .. "/.git") == 1 then
+      return dir
+    end
+    dir = vim.fn.fnamemodify(dir, ":h")
+  end
+
+  return current_dir
+end
+
+vim.g.vdebug_options = {
+  port = 9003,
+  path_maps = {
+    ["/var/www/html"] = find_project_root()
+  }
+};
