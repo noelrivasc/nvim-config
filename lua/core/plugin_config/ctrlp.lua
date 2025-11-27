c004101e04d2787db22ee3c89d22c23f8eacec53
Noel Rivas <hola@noelr.dev>
2025-11-27 11:39:21 -0600

    Added config to ignore .gitignored elements in CtrlP

--- lua/core/plugin_config/ctrlp.lua
+++ lua/core/plugin_config/ctrlp.lua
@@ -1,4 +1,7 @@
-vim.opt.wildignore:append{'*/vendor/*', '.git/*', '*/node_modules/*'}
+vim.opt.wildignore:append { '*/vendor/*', '.git/*', '*/node_modules/*', '*/_site/*' }
+-- Ignore git-ignored files
+vim.g.ctrlp_user_command = { '.git', 'cd %s && git ls-files -co --exclude-standard' }
+
 -- Sets the root of CtrlP to the current file, or
 -- the directory where vim was started. This is
 -- to prevent it from scanning everything under
